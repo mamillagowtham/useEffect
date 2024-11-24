7d91c5d0c8907ea62ce51737e8e1ff968c1b89d7
mamillagowtham <mamillagowtham@gmail.com>
2024-11-24 22:07:46 +0530

    useEffect

--- useEffect(crud)/my-vue-app/src/App.jsx
+++ useEffect(crud)/my-vue-app/src/App.jsx
@@ -5,13 +5,16 @@ const App = () => {
 
 const [timer,setTimer] = useState(true)
   return (
+    // <>
+        
+    //     {timer && <Timer/>} 
+    //     <button onClick={()=> setTimer(!timer)}>
+    //       {timer ? "hide" : "show"}
+    //     </button>
+        
+    // </>
     <>
-        
-        {timer && <Timer/>} 
-        <button onClick={()=> setTimer(!timer)}>
-          {timer ? "hide" : "show"}
-        </button>
-        
+    
     </>
   )
 }
